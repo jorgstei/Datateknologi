7cbbb0024d8f8e8f8ed1106a5dfb63edc25b8e2b
jorgstei <jorgstei@ntnu.no>
2021-08-26 00:05:48 +0200

    Absolute fail in assignment 1 TDT4200, gl tomorrow

--- TDT4200/Assignment_1/Assignment/c-intro/main.c
+++ TDT4200/Assignment_1/Assignment/c-intro/main.c
@@ -10,14 +10,14 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
-//#include <windows.h>
+#include <windows.h>
 //#include <magick_wand.h>
 
 typedef struct{
     unsigned char r;
     unsigned char g;
     unsigned char b;
-    unsigned char a;
+// unsigned char a;
 } pixel;
 
 int main(int argc, char** argv)
@@ -34,22 +34,28 @@ int main(int argc, char** argv)
     printf("height:%d, width: %d\n", height, width);
     if (char_pixels_1 == NULL || char_pixels_2 == NULL)
     {
+        printf("if was true");
         exit(1);
     }
-
+    
     //TODO 2 - typecast pointer
-    pixel* pixels_1;
-    pixel* pixels_2;
+    pixel* pixels_1 = (pixel* ) char_pixels_1;
+    pixel* pixels_2 = (pixel* ) char_pixels_2;
 
     //TODO 3 - malloc
-    pixel* pixels_out = (pixel *) char_pixels_1;
+    pixel* pixels_out = (pixel*)malloc(sizeof(pixel)*width*height);
 
     //TODO 4 - loop
-    //Write your loop here
+    printf("%c\n", char_pixels_1);
+    int i;
+    for (i = 0; i < sizeof(char_pixels_1); i++){ 
+        //printf("%d",i);
+    }
 
-    stbi_write_png("output.png", width, height, STBI_rgb_alpha, pixels_out, sizeof(pixel) * width);
+    stbi_write_png("output.png", width, height, STBI_rgb_alpha, char_pixels_1, sizeof(pixel) * width);
 
     //TODO 5 - free
-
+    free(pixels_out);
+    printf("Ending");
     return 0;
 }
